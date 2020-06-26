86878391568893557b85f80649b0b7eee05b56ab
amitcjc1 <61121166+amitcjc1@users.noreply.github.com>
2020-06-26 10:24:20 +0530

    Update Demo.java

--- DemoGit/src/Demo.java
+++ DemoGit/src/Demo.java
@@ -4,7 +4,7 @@ public class Demo
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git Local");
-		System.out.println("Hello");
+		System.out.println("Hello==========================CJC");
 		
 		System.out.println("Hello GIt Server");
 		
